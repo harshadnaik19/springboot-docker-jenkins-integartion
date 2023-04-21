e3d1ae16ac583c6cfeb762f4e4640b73eae40850
hnaik1 <harshad.naik@morningstar.com>
2023-04-21 12:22:58 +0530

    first commit

---
+++ src/test/java/com/mstar/SpringDockerJenkinsIntegration/SpringDockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstar.SpringDockerJenkinsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
